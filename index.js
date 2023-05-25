3cd701804aed1c98547e7a2fc594ce8ec83ac169
sameer-shaikhdev <130742021+sameer-shaikhdev@users.noreply.github.com>
2023-05-25 10:59:10 +0000

    adding comments

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// markOne How well do you know Sameer
+
 var readlineSync = require('readline-sync');
 
 
@@ -19,15 +21,24 @@ var score = 0;
 //array of qbject
 var questionAnswer = [{
   question: "what is sameer's lasts name:",
-     answer: "shaikh"},
-  {question: "what is sameer's fathers name:",
-     answer: "amin"},
-  {question: "where does sameer lives:",
-     answer: "latur"},
-  {question: "how old is sameer:",
-     answer: 25},
-  {question: "name the fruit that sameer likes:",
-     answer: "grapes"},  
+  answer: "shaikh"
+},
+{
+  question: "what is sameer's fathers name:",
+  answer: "amin"
+},
+{
+  question: "where does sameer lives:",
+  answer: "latur"
+},
+{
+  question: "how old is sameer:",
+  answer: 25
+},
+{
+  question: "name the fruit that sameer likes:",
+  answer: "grapes"
+},
 ]
 
 //opration funtion :check if the answer is correct or not;
@@ -41,25 +52,25 @@ function operation(ans, ques) {
 }
 
 //welcome function: welcome and get the username;
-function welcome(){
-console.log("Do you Know Me??");
-var userName= readlineSync.question("What is your name: ");
-console.log("welcome to sameer's quiz "+ userName,);
+function welcome() {
+  console.log("Do you Know Me??");
+  var userName = readlineSync.question("What is your name: ");
+  console.log("welcome to sameer's quiz " + userName,);
 }
 
 //function quiz: ask question and take answers from the player
-function quiz(){
-for (var i = 0; i < questionAnswer.length; i++) {
-  var answer = readlineSync.question(questionAnswer[i].question);
-  operation(answer, i);
-}
+function quiz() {
+  for (var i = 0; i < questionAnswer.length; i++) {
+    var answer = readlineSync.question(questionAnswer[i].question);
+    operation(answer, i);
+  }
   console.log("your final score is:", +score);
   highscore.scoreProcess();
 }
 
 
- // initailaizing welcome function
- welcome();
+// initailaizing welcome function
+welcome();
 
 // initailizing quiz function
 quiz();
